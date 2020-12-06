3d1783b84ed9d7363cbe8add8e0f5bd81c86dd8d
Ulmasbek Akhtamov <akhtamovulmasbek@gmail.com>
2020-12-06 03:03:56 -0800

    Initial Commit

---
+++ Movie Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Info
+//
+//  Created by O’lmasbek Axtamov on 12/6/20.
+//  Copyright © 2020 O’lmasbek Axtamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
